f4874d6e695818eb547985d24ac995cc6b697dd6
kakaoracle <kakaoracle@163.com>
2020-08-09 23:18:26 +0800

    udpate

---
+++ java/com/kaka/MyClassLoader.java
@@ -0,0 +1,76 @@
+package com.kaka;
+
+
+import java.io.*;
+/*
+*
+* */
+public class MyClassLoader extends ClassLoader {
+    private String name;
+
+    private ClassLoader classLoader;
+
+    @Override
+    protected Class<?> findClass(String name) {
+        byte[] data1 = getBytes("D:\\githubrepository\\axiom_tomcat\\com\\kaka\\Test1.class");
+
+        Class<?> clazz = this.defineClass(name, data1, 0, data1.length);
+        return  clazz;
+    }
+
+    public MyClassLoader(ClassLoader classLoader, String name) {
+        super(classLoader);
+        this.name = name;
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        ClassLoader system = getSystemClassLoader().getParent();
+
+        Class<?> clazz = system.loadClass(name);
+
+        if (clazz != null) {
+            return clazz;
+        }
+
+        return findClass(name);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        // myClassLoader是自定义的,因此父加载器是application加载器
+        MyClassLoader myClassLoader = new MyClassLoader(MyClassLoader.class.getClassLoader(), "MyClassLoader");
+        Class<?> clazz = myClassLoader.loadClass("com.kaka.Test1");
+        System.out.println(clazz.getClassLoader());
+    }
+
+
+    public byte[] getBytes(String filePath)  {
+        InputStream is = null;
+        byte[] data = null;
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            is = new FileInputStream(new File(filePath));
+            int c = 0;
+            while ((c=is.read()) != -1){
+                baos.write(c);
+            }
+            data = baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+                baos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return data;
+
+    }
+
+
+}
---
+++ java/com/kaka/Test1.java
@@ -0,0 +1,7 @@
+package com.kaka;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int i = 0;
+    }
+}
